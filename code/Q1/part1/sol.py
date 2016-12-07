628fc2bfdcd91f87cdded3bf36b9926278dbdb77
Abhishek Jaisingh <abhi2254015@gmail.com>
2016-12-07 09:46:08 +0530

    refactor: improved day 1 code

--- code/Q1/part1/sol.py
+++ code/Q1/part1/sol.py
@@ -1,50 +1,36 @@
 with open("input.txt", "r") as f:
-	s = f.read().strip().split(", ")
-	s = [ (move[0], int(move[1:])) for move in s]
+	moves = f.read().strip().split(", ")
+	moves = [ (move[0], int(move[1:])) for move in moves]
 
-# print(s)
-
-current = "N"
 x = 0
 y = 0
+current = 0 # current direction
+# NORTH = 0, EAST = 1
+# WEST = 2, SOUTH = 4
 
-for move in s:
+for move in moves:
 	direction = move[0]
 	steps = move[1]
-	
-	if current == "N":
-		if direction == "L":
-			current = "W"
-		if direction == "R":
-			current = "E"
-	
-	elif current == "S":
-		if direction == "L":
-			current = "E"
-		if direction == "R":
-			current = "W"
-	
-	elif current == "E":
-		if direction == "L":
-			current = "N"
-		if direction == "R":
-			current = "S"
 
-	elif current == "W":
-		if direction == "L":
-			current = "S"
-		if direction == "R":
-			current = "N"
+	if direction == "L":
+		current  = ( current-1 )% 4
+	if direction == "R":
+		current  = ( current+1 )% 4
 
-	if current == "N":
-		y += steps
-	if current == "S":
-		y -= steps
-	if current == "E":
-		x += steps
-	if current == "W":
-		x -= steps
+	if current == 0: # NORTH
+		for i in range(steps):
+			y += 1
+	if current == 1: # EAST
+		for i in range(steps):
+			x += 1
+	if current == 2: # SOUTH
+		for i in range(steps):
+			y -= 1
+	if current == 3: # WEST
+		for i in range(steps):
+			x -= 1
+
 
 blocks = abs(x) + abs(y)
-# print(x, y)
-print(blocks)		
+print(blocks)
+					
--- code/Q1/part2/sol.py
+++ code/Q1/part2/sol.py
@@ -1,10 +1,58 @@
 with open("input.txt", "r") as f:
-	s = f.read().strip().split(", ")
-	s = [ (move[0], int(move[1:])) for move in s]
+	moves = f.read().strip().split(", ")
+	moves = [ (move[0], int(move[1:])) for move in moves]
 
-direction = 0
-# NORTH = 0
-# EAST = 1
-# WEST =
+x = 0
+y = 0
+current = 0 # current direction
+# NORTH = 0, EAST = 1
+# WEST = 2, SOUTH = 4
 
-print(-1%4)
+visited = []
+visited.append( (x, y) )
+
+for move in moves:
+	direction = move[0]
+	steps = move[1]
+
+	found = False
+	
+	if direction == "L":
+		current  = ( current-1 )% 4
+	if direction == "R":
+		current  = ( current+1 )% 4
+
+	if current == 0: # NORTH
+		for i in range(steps):
+			y += 1
+			if (x, y) in visited:
+				found = True
+				break
+			visited.append((x, y))
+	if current == 1: # EAST
+		for i in range(steps):
+			x += 1
+			if (x, y) in visited:
+				found = True
+				break
+			visited.append((x, y))
+	if current == 2: # SOUTH
+		for i in range(steps):
+			y -= 1
+			if (x, y) in visited:
+				found = True
+				break
+			visited.append((x, y))
+	if current == 3: # WEST
+		for i in range(steps):
+			x -= 1
+			if (x, y) in visited:
+				found = True
+				break
+			visited.append((x, y))
+	if found:
+		break
+
+blocks = abs(x) + abs(y)
+print(blocks)
+					
