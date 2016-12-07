0f6d3d99011f31a4c0bcda203b80768f81fde7af
Abhishek Jaisingh <abhi2254015@gmail.com>
2016-12-08 02:35:38 +0530

    add: problem 6

---
+++ code/Q6/part2/sol.py
@@ -0,0 +1,24 @@
+with open("input.txt") as f:
+	msg = f.readlines()
+	msg = [line.strip() for line in msg]
+
+r = len(msg)
+c = len(msg[0])
+
+print(r, c)
+print(msg)
+
+ans = ""
+
+for j in range(c):
+	freq = [0 for a in range(26)]
+	for i in range(r):
+		ch = ord(msg[i][j])-97
+		freq[ch] += 1
+	index = 0
+	for i in range(26):
+		if freq[i] < freq[index] and freq[i] != 0:
+			index = i
+	ans += chr(index+97)
+
+print(ans)
